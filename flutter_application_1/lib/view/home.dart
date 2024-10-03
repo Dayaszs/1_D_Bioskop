2090e0593932bacb58c0ad5f99302db116a1400d
CumaBisaHelloWorld <ricoadja52@gmail.com>
2024-10-03 15:08:51 +0700

    profileBaru

--- flutter_application_1/lib/view/home.dart
+++ flutter_application_1/lib/view/home.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utilities/constant.dart';
+import 'package:flutter_application_1/view/profile.dart';
 import 'package:flutter_application_1/view/home.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+  final Map<String, dynamic> userData; // Add this to pass the user data
+
+  const HomeView({super.key, required this.userData}); // Update constructor
 
   @override
   State<HomeView> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
-   int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -21,19 +24,21 @@ class _HomeViewState extends State<HomeView> {
       case 0:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => HomeView()),
+          MaterialPageRoute(builder: (context) => HomeView(userData: widget.userData)),
         );
         break;
       case 1:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => ListView()), //ganti kelas list
+          MaterialPageRoute(builder: (context) => ListView()), // Replace with actual list view
         );
         break;
       case 2:
         Navigator.push(
           context,
-          // MaterialPageRoute(builder: (context) => ShowProfile()), //ganti kelas profile
+          MaterialPageRoute(
+            builder: (context) => ShowProfile(data: widget.userData), // Pass user data to ShowProfile
+          ),
         );
         break;
     }
@@ -49,18 +54,18 @@ class _HomeViewState extends State<HomeView> {
           backgroundColor: lightColor,
         ),
         body: Center(
-          child: // ini isi kode ipann
+          child: Text('Home Content'), // Replace with actual home content
         ),
         bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: lightColor,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+          backgroundColor: lightColor,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
