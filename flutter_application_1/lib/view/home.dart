2e4da1fe7b5685bbd54c45f1927988c38d56964a
AmateurLinuxUser <220711708@students.uajy.ac.id>
2024-10-03 15:25:39 +0700

    update home

--- flutter_application_1/lib/view/home.dart
+++ flutter_application_1/lib/view/home.dart
@@ -10,7 +10,8 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-   int _selectedIndex = 0;
+  int _selectedIndex = 0;
+  final TextEditingController _searchController = TextEditingController();
 
   void _onItemTapped(int index) {
     setState(() {
@@ -21,20 +22,20 @@ class _HomeViewState extends State<HomeView> {
       case 0:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => HomeView()),
+          MaterialPageRoute(builder: (context) => const HomeView()),
         );
         break;
       case 1:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ListView()), //ganti kelas list
-        );
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => ListView()), //ganti kelas list
+        // );
         break;
       case 2:
-        Navigator.push(
-          context,
-          // MaterialPageRoute(builder: (context) => ShowProfile()), //ganti kelas profile
-        );
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => ShowProfile()), //ganti kelas profile
+        // );
         break;
     }
   }
@@ -45,23 +46,110 @@ class _HomeViewState extends State<HomeView> {
       child: Scaffold(
         backgroundColor: darkColor,
         appBar: AppBar(
-          title: const Text('Home'),
-          backgroundColor: lightColor,
+        backgroundColor: lightColor, // Warna latar belakang AppBar
+        title: Text(
+          'Atma Teater', // Judul di AppBar
+          style: textStyle6, // Styling teks judul
         ),
+        centerTitle: true, // Membuat judul di tengah
+        automaticallyImplyLeading: false, // Menghilangkan tombol back // Menghilangkan tombol back
+      ),
         body: Center(
-          child: // ini isi kode ipann
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 370,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: "Search Film Here", // Teks hint
+                      prefixIcon: Icon(Icons.search), // Ikon search di sebelah kiri
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.clear), // Tombol untuk clear input
+                        onPressed: () {
+                          _searchController.clear(); // Aksi untuk clear text input
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0), // Radius border
+                        borderSide: BorderSide.none, // Hilangkan garis border
+                      ),
+                      filled: true, // Background terisi warna
+                      fillColor: Colors.grey[200], // Warna background search bar
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0), // Padding dalam search bar
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    color: lightColor,
+                  ),
+                ),
+              ),
+
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 12.0, left: 12.0, bottom: 12.0),
+                    child: Text("Film Populer Saat Ini : ", style: textStyle2),
+                  ),
+                ],
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 150,
+                        color: lightColor,
+                      ),
+                      Container(
+                        width: 100,
+                        height: 150,
+                        color: lightColor,
+                      ),
+                      Container(
+                        width: 100,
+                        height: 150,
+                        color: lightColor,
+                      ),
+                    ],
+                  ),
+
+                ],
+              )
+            ],
+          ),
         ),
         bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: lightColor,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+          backgroundColor: lightColor,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
 }
+
