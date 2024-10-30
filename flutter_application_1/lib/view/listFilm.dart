102029a144ec7346ad653a29bcec50432e3a2143
Kevin Philips Tanamas <220711789@students.uajy.ac.id>
2024-10-30 15:24:51 +0700

    perbaiki navigasi

--- flutter_application_1/lib/view/listFilm.dart
+++ flutter_application_1/lib/view/listFilm.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data/film.dart';
 import 'package:flutter_application_1/view/home.dart';
--- flutter_application_1/lib/view/profile.dart
+++ flutter_application_1/lib/view/profile.dart
@@ -19,39 +19,38 @@ class ShowProfile extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Circular profile picture with border
               CircleAvatar(
                 radius: 50,
                 backgroundColor: const Color.fromRGBO(255, 193, 7, 1),
                 child: CircleAvatar(
                   radius: 48,
-                  backgroundImage: data['profile_image'] != null 
-                      ? FileImage(File(data['profile_image'])) // Gambar dari file lokal
-                      : AssetImage('assets/placeholder.png') as ImageProvider, // Placeholder jika tidak ada gambar
+                  backgroundImage: (data['profile_image'] != null && 
+                                  File(data['profile_image']).existsSync())
+                      ? FileImage(File(data['profile_image']))
+                      : const AssetImage('assets/placeholder.png'),
+                  // Periksa dan gunakan placeholder jika tidak ada gambar
                 ),
               ),
               const SizedBox(height: 20),
-              // Display the name and email
               Text(
-                data['username'], // Username dari registrasi
+                data['username'] ?? 'Nama tidak tersedia',
                 style: const TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white
+                  color: Colors.white,
                 ),
               ),
               const SizedBox(height: 10),
               Text(
-                data['email'], // Email dari registrasi
+                data['email'] ?? 'Email tidak tersedia',
                 style: const TextStyle(
                   fontSize: 16,
                   color: Colors.white,
                 ),
               ),
               const SizedBox(height: 20),
-              // Display phone number
               Text(
-                data['nomor_telepon'], // Nomor telepon dari registrasi
+                data['nomor_telepon'] ?? 'Nomor telepon tidak tersedia',
                 style: const TextStyle(
                   fontSize: 16,
                   color: Colors.grey,
