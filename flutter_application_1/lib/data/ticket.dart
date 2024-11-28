57688095cb6c1cb3b78f1815079440769b08d128
Dhiaz Juan Richard Lagarense <220711695@students.uajy.ac.id>
2024-11-28 11:28:45 +0700

    PDF DAH JADI

--- flutter_application_1/lib/data/ticket.dart
+++ flutter_application_1/lib/data/ticket.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_application_1/data/film.dart';
 
 class Ticket {
+  final int id;
   final String picture;
   final String judul;
   final String genre;
   final String status;  // "Completed" or "Not Watched"
 
   Ticket({
+    required this.id,
     required this.picture,
     required this.judul,
     required this.genre,
@@ -14,11 +16,15 @@ class Ticket {
   });
 }
 
-final List<Ticket> tickets = films.map((film) {
-  return Ticket(
-    picture: film.picture,
-    judul: film.judul,
-    genre: film.genre,
-    status: 'Not Watched',  // Assuming initially all films are "Not Watched"
+final List<Ticket> tickets = films.asMap().map((index, film) {
+  return MapEntry(
+    index,
+    Ticket(
+      id: 1,
+      picture: film.picture,
+      judul: film.judul,
+      genre: film.genre,
+      status: index.isEven ? 'Completed' : 'Not Watched',  // Set status based on even/odd index
+    ),
   );
-}).toList();
+}).values.toList();
